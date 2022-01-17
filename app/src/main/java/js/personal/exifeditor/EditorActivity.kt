f1c71ff08446dbcb2485e19ad6e2d781eddb579a
LeeJoonHee <neo81389@gmail.com>
2022-01-17 17:14:29 +0900

    feat(main): EditorActivity added & temporary .xml file deleted

---
+++ app/src/main/java/js/personal/exifeditor/EditorActivity.kt
@@ -0,0 +1,11 @@
+package js.personal.exifeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.editor_main)
+    }
+}
